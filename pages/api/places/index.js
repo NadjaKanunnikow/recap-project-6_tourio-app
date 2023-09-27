d3067e954d4c2f5f39ba8825c66d7c107779a6cc
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-09-27 10:02:23 +0200

    add form

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -8,4 +8,16 @@ export default async function handler(request, response) {
 
     return response.status(200).json(place);
   }
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
+
+      console.log("placedat", placeData);
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
 }
